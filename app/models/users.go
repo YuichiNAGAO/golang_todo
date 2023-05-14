3717cc9b8f16d5761e336c6b8365a87007defe10
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-05-15 08:42:32 +0900

    CreatedAtを取れるように

--- app/models/users.go
+++ app/models/users.go
@@ -31,8 +31,19 @@ func (u *User) CreateUser() (err error) {
 
 func GetUser(id int) (user User, err error) {
 	user = User{}
-	cmd := `select id, uuid, name, email, password from users where id = ?`
+	cmd := `select id, uuid, name, email, password, created_at from users where id = ?`
 
-	err = Db.QueryRow(cmd, id).Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.Password)
+	var created_at []uint8
+
+	err = Db.QueryRow(cmd, id).Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.Password, &created_at)
+
+	created_at_str := string(created_at)                                      // []uint8 を string に変換する
+	created_at_time, err := time.Parse("2006-01-02 15:04:05", created_at_str) // string を time.Time に変換する
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	user.CreatedAt = created_at_time // 変換した created_at の値を user.CreatedAt に代入する
+
 	return user, err
 }
